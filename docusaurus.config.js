1441706feec254fd11a9b4f0d251d1a0f80d99f0
Topper Bowers <topper@toppingdesign.com>
2022-05-12 15:55:41 +0200

    google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
+                gtag: {
+                    trackingID: 'G-EZNN72Y8DV',
+                    anonymizeIP: true,
+                },
             }),
         ],
     ],
